f975a5093f318a366b21c6c34a547d6075ffa21a
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-07-09 11:49:34 +0900

    image_tag,user詳細修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "prototypes#index"
+  root to: 'prototypes#index'
   
   resources :prototypes do
     resources :comments, only: :create
